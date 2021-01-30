cfbe0f6a5eb143614028161939fe81e3a06672cc
FleeingSunlight <gh@fleeingsunlight.dev>
2021-01-30 16:12:11 +0700

    feat(sym.py): :tada:

---
+++ sym.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import os
+
+base = os.path.dirname(__file__)
+
+os.chdir(base)
+
+apps = [
+    "./apps/konachan",
+    "./apps/danbooru"
+]
+
+def sym(dst):
+    os.chdir(base)
+    os.chdir(dst)
+    depth = len(dst.split("/")) - 1
+    relative_path = "../" * depth + "apis"
+    os.symlink(relative_path, "apis")
+
+for app in apps:
+    sym(app)

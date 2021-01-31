5a266c47b6c7a9e4c83b90b49d8f5c0b515775c0
FleeingSunlight <gh@fleeingsunlight.dev>
2021-01-31 14:34:49 +0700

    feat(apps/nhentai): init Maven project

---
+++ apps/nhentai/src/main/java/trif0lium/stellarish/nhentai/App.java
@@ -0,0 +1,13 @@
+package trif0lium.stellarish.nhentai;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
